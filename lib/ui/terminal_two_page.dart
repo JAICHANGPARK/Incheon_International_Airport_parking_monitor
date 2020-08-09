f1bd4cd262b559a8c96150c044b6a25e88507be4
Dreamwalker <aristojeff@gmail.com>
2020-08-09 14:04:59 +0900

    Create terminal_two_page.dart

---
+++ lib/ui/terminal_two_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TerminalTwoPage extends StatefulWidget {
+  @override
+  _TerminalTwoPageState createState() => _TerminalTwoPageState();
+}
+
+class _TerminalTwoPageState extends State<TerminalTwoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
