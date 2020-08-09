09523ff68529109441dadfa40e16dc9c5d399e85
Dreamwalker <aristojeff@gmail.com>
2020-08-09 13:37:21 +0900

    Update home_page.dart

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:incheon_airport_parking/model/parking.dart';
+import 'package:incheon_airport_parking/res/api.dart';
+import 'package:incheon_airport_parking/res/service_key.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -9,6 +12,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+      future: fetchParkingData(APIServiceKey.API_KEY),
+      builder: (context, snapShot){
+        if(snapShot.hasData){
+          List<Parking> _items = snapShot.data;
+          return ListView.builder(
+            itemCount: _items.length,
+            itemBuilder: (context, index){
+              return ListTile(title: Text(_items[index].floor),);
+            },);
+        }else{
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
