ae87970796ba8e95524e8d926250073af062ec9e
Dreamwalker <aristojeff@gmail.com>
2020-08-09 11:42:31 +0900

    Create parking.dart

---
+++ lib/model/parking.dart
@@ -0,0 +1,7 @@
+class Parking{
+  final String datetm;
+  final String floor;
+  final String parking;
+  final String parkingArea;
+  Parking(this.datetm, this.floor, this.parking, this.parkingArea);
+}
