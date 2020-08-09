99fbf3a38df7a903e917afc2fa3e6d836b4be207
Dreamwalker <aristojeff@gmail.com>
2020-08-09 13:16:07 +0900

    Update terminal_one_page.dart

--- lib/ui/terminal_one_page.dart
+++ lib/ui/terminal_one_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class TerminalOnePage extends StatefulWidget {
+  @override
+  _TerminalOnePageState createState() => _TerminalOnePageState();
+}
+
+class _TerminalOnePageState extends State<TerminalOnePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
