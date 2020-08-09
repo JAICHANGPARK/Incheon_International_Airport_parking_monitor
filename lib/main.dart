1424ba989c1465e9078bf5528fcc720c37b4a340
Dreamwalker <aristojeff@gmail.com>
2020-08-09 12:16:16 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:incheon_airport_parking/res/api.dart';
 import 'package:incheon_airport_parking/res/service_key.dart';
+import 'package:line_icons/line_icons.dart';
 
 import 'model/parking.dart';
 
@@ -45,8 +47,10 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
 //    fetchParkingData(APIServiceKey.API_KEY);
   }
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
      
       body:SafeArea(
@@ -84,11 +88,52 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: _incrementCounter,
+//        tooltip: 'Increment',
+//        child: Icon(Icons.add),
+//      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+        ]),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+                gap: 8,
+                activeColor: Colors.white,
+                iconSize: 24,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                duration: Duration(milliseconds: 800),
+                tabBackgroundColor: Colors.grey[800],
+                tabs: [
+                  GButton(
+                    icon: LineIcons.home,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: Icons.local_parking_sharp,
+                    text: 'Likes',
+                  ),
+                  GButton(
+                    icon: Icons.local_parking_sharp,
+                    text: 'Search',
+                  ),
+                  GButton(
+                    icon: Icons.local_parking_sharp,
+                    text: 'Profile',
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onTabChange: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                }),
+          ),
+        ),
+      ),
     );
   }
 }
