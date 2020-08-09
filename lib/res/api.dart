ac8e4b96cdfbe3bc40b9de48aeeee42451e64f2e
Dreamwalker <aristojeff@gmail.com>
2020-08-09 13:37:18 +0900

    Update api.dart

--- lib/res/api.dart
+++ lib/res/api.dart
@@ -25,3 +25,28 @@ Future<List<Parking>> fetchParkingData(String serviceKey) async {
     return null;
   }
 }
+
+Future<List<Parking>> fetchParkingTerminalOneData(String serviceKey) async {
+  var response = await http.get("http://openapi.airport.kr/openapi/service/StatusOfParking/getTrackingParking"
+      "?ServiceKey=$serviceKey");
+
+  if (response.statusCode == HttpStatus.ok) {
+//    print(response.body);
+    final document = XmlDocument.parse(utf8.decode(response.bodyBytes));
+//    print(document.toString());
+    List<Parking> _parkingList = [];
+    var _item =document.findAllElements("item");
+    _item.forEach((element) {
+      String _floor = element.findElements("floor").first.text;
+      List<String> _floorFilter = _floor.split(" ");
+      print(_floorFilter.first);
+      if(_floorFilter.first == "T1"){
+        _parkingList.add(Parking(  element.findElements("datetm").first.text, element.findElements("floor").first.text, element.findElements("parking").first.text,
+            element.findElements("parkingarea").first.text));
+      }
+    });
+    return _parkingList;
+  }else{
+    return null;
+  }
+}
