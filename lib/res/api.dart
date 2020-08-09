aac91898da544ee1e838e10aee96bfa1972da352
Dreamwalker <aristojeff@gmail.com>
2020-08-09 11:32:47 +0900

    Update api.dart

---
+++ lib/res/api.dart
@@ -0,0 +1,12 @@
+import 'dart:html';
+
+import 'package:http/http.dart' as http;
+
+Future getParkingData(String serviceKey) async {
+  var response = await http.get("http://openapi.airport.kr/openapi/service/StatusOfParking/getTrackingParking"
+      "?ServiceKey=$serviceKey");
+
+  if (response.statusCode == HttpStatus.ok) {
+    print(response.body);
+  }
+}
